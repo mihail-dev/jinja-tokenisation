1d095427293419da5040f09a75b188117ef9d253
Mihail Ivanov <mihail.e.ivanov@gmail.com>
2016-06-23 22:26:06 +0100

    Dockerised tokenisation script with jinja

---
+++ Dockerfile
@@ -0,0 +1,8 @@
+FROM centos:latest
+
+RUN curl "https://bootstrap.pypa.io/get-pip.py" -o "get-pip.py" && python get-pip.py && rm -f get-pip.py && pip install jinja2
+
+COPY templates /opt/templates
+COPY tokenisation.py /opt
+
+CMD /bin/bash
---
+++ tokenisation.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+
+import os
+from jinja2 import Template, Environment, FileSystemLoader
+
+env = Environment(loader=FileSystemLoader('/opt/templates'))
+
+# Load environment variables
+env_vars_string = "tomcat_generaloptions tomcat_debugjavaoptions standalone_javaoptions"
+env_vars_list = env_vars_string.split()
+env_vars_dict = {}
+
+# Add environment variables to a dict
+for env_var in env_vars_list:
+	env_key = env_var
+	env_value = os.environ[env_var]
+	env_vars_dict[env_key] = env_value
+
+# Load templates file
+template = env.get_template('local.properties.template')
+
+# Tokenise
+output_from_parsed_template = template.render(env_vars_dict)
+
+print 'Tokenisation complete'
+
+# save file
+# TODO: append a file instead
+with open('local.properties', "wb") as tokenised_file:
+    tokenised_file.write(output_from_parsed_template)
+
+
