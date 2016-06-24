beb640b498ef5b8e14fb0c940e5cc166588957ae
Ivanov, Mihail <mihail.ivanov@accenture.com>
2016-06-24 12:03:35 +0100

    Adding options to tokenisation script

---
+++ README.md
@@ -0,0 +1,3 @@
+Example usage:
+
+python tokenisation.py -e 'tomcat_generaloptions tomcat_debugjavaoptions standalone_javaoptions' -d '/opt/templates' -f 'local.properties.template' -o 'local.properties'
--- tokenisation.py
+++ tokenisation.py
@@ -2,30 +2,56 @@
 
 import os
 from jinja2 import Template, Environment, FileSystemLoader
+import sys, getopt
 
-# Load environment variables
-env_vars_string = "tomcat_generaloptions tomcat_debugjavaoptions standalone_javaoptions"
-env_vars_list = env_vars_string.split()
-env_vars_dict = {}
+def main(argv):
+	# Fetch options
+	env_vars_string = ''
+	template_dir = ''
+	template_file = ''
+	output_file = ''
 
-# Add environment variables to a dict
-for env_var in env_vars_list:
-	env_key = env_var
-	env_value = os.environ[env_var]
-	env_vars_dict[env_key] = env_value
+	try:
+		opts, args = getopt.getopt(argv,"he:d:f:o:",["env_vars_string=", "template_dir=", "template_file=", "output_file="])
+	except getopt.GetoptError:
+		print 'Example usage:' + '\n' + 'tokenisation.py -e ENV_VARS_STRING -d TEMPLATE_DIR -f TEMPLATE_FILE -o OUTPUT_FILE'
+		sys.exit(2)
+	for opt, arg in opts:
+		if opt == '-h':
+			print 'Example usage:' + '\n' + 'tokenisation.py -e ENV_VARS_STRING -d TEMPLATE_DIR -f TEMPLATE_FILE -o OUTPUT_FILE'
+			sys.exit()
+		elif opt in ("-e", "--env_vars_string"):
+			env_vars_string = arg
+		elif opt in ("-d", "--template_dir"):
+			template_dir = arg
+		elif opt in ("-f", "--template_file"):
+			template_file = arg
+		elif opt in ("-o", "--output_file"):
+			output_file = arg
 
-# Load templates file
-env = Environment(loader=FileSystemLoader('/opt/templates'))
-template = env.get_template('local.properties.template')
+	# Load environment variables
+	env_vars_list = env_vars_string.split()
+	env_vars_dict = {}
 
-# Tokenise
-output_from_parsed_template = template.render(env_vars_dict)
+	# Add environment variables to a dict
+	for env_var in env_vars_list:
+		env_key = env_var
+		env_value = os.environ[env_var]
+		env_vars_dict[env_key] = env_value
 
-print 'Tokenisation complete'
+	# Load templates file
+	env = Environment(loader=FileSystemLoader(template_dir))
+	template = env.get_template(template_file)
 
-# save file
-# TODO: append a file instead
-with open('local.properties', "wb") as tokenised_file:
-    tokenised_file.write(output_from_parsed_template)
+	# Tokenise
+	output_from_parsed_template = template.render(env_vars_dict)
 
+	print 'Tokenisation complete'
 
+	# Save file
+	# TODO: append a file instead
+	with open(output_file, "wb") as tokenised_file:
+	    tokenised_file.write(output_from_parsed_template)
+
+if __name__ == "__main__":
+	main(sys.argv[1:])
